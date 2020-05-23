cec7aecdbba54e3a719882cc8bdd1f2d8fe19e0a
Kirill Minko <kirill040994@gmail.com>
2020-05-24 00:45:44 +0300

    show fake data using ArrayAdapter

--- app/src/main/java/com/example/quakereport/MainActivity.kt
+++ app/src/main/java/com/example/quakereport/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.quakereport
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val quakes = listOf(
+        "San Francisco",
+        "London",
+        "Tokyo",
+        "Mexico City",
+        "Moscow",
+        "Rio de Janeiro",
+        "Paris"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        quakes_list_view.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, quakes)
     }
 }

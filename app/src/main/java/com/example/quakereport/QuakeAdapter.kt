63d5487ebc192bbff6d2d4ba80010fc3e59e8a05
Kirill Minko <kirill040994@gmail.com>
2020-05-24 16:15:18 +0300

    encapsulate logic of onBindViewHolder into QuakeViewHolder

--- app/src/main/java/com/example/quakereport/QuakeAdapter.kt
+++ app/src/main/java/com/example/quakereport/QuakeAdapter.kt
@@ -21,53 +21,54 @@ class QuakeAdapter(private val quakes: List<Quake>) : RecyclerView.Adapter<Quake
 
     override fun onBindViewHolder(holder: QuakeViewHolder, position: Int) {
         val item = quakes[position]
-        val itemView = holder.itemView
-
-        DrawableCompat.setTint(
-            DrawableCompat.wrap(itemView.magnitude.background),
-            ContextCompat.getColor(itemView.context, getMagnitudeColor(item.magnitude))
-        )
-        itemView.magnitude.text = item.magnitude.toString()
-
-        val originalLocation = item.place
-        val locationSeparator = " of "
-        val locationOffset: String
-        val primaryLocation: String
-        if (originalLocation.contains(locationSeparator)) {
-            val parts = originalLocation.split(locationSeparator)
-            locationOffset = parts[0] + locationSeparator
-            primaryLocation = parts[1]
-        } else {
-            locationOffset = "Near the"
-            primaryLocation = originalLocation
-        }
-        itemView.location_offset.text = locationOffset
-        itemView.primary_location.text = primaryLocation
-
-        val datetime = Date(item.time)
-        itemView.date.text = SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH).format(datetime)
-        itemView.time.text = SimpleDateFormat("HH:mm", Locale.ENGLISH).format(datetime)
+        holder.bind(item)
     }
 
     class QuakeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    }
+        fun bind(item: Quake) {
+            DrawableCompat.setTint(
+                DrawableCompat.wrap(itemView.magnitude.background),
+                ContextCompat.getColor(itemView.context, getMagnitudeColor(item.magnitude))
+            )
+            itemView.magnitude.text = item.magnitude.toString()
 
-    private fun getMagnitudeColor(magnitude: Double): Int {
-        val magnitudeColorResourceId: Int
-        val magnitudeFloor = Math.floor(magnitude).toInt()
-        magnitudeColorResourceId = when (magnitudeFloor) {
-            0, 1 -> R.color.magnitude1
-            2 -> R.color.magnitude2
-            3 -> R.color.magnitude3
-            4 -> R.color.magnitude4
-            5 -> R.color.magnitude5
-            6 -> R.color.magnitude6
-            7 -> R.color.magnitude7
-            8 -> R.color.magnitude8
-            9 -> R.color.magnitude9
-            else -> R.color.magnitude10plus
+            val originalLocation = item.place
+            val locationSeparator = " of "
+            val locationOffset: String
+            val primaryLocation: String
+            if (originalLocation.contains(locationSeparator)) {
+                val parts = originalLocation.split(locationSeparator)
+                locationOffset = parts[0] + locationSeparator
+                primaryLocation = parts[1]
+            } else {
+                locationOffset = "Near the"
+                primaryLocation = originalLocation
+            }
+            itemView.location_offset.text = locationOffset
+            itemView.primary_location.text = primaryLocation
+
+            val datetime = Date(item.time)
+            itemView.date.text = SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH).format(datetime)
+            itemView.time.text = SimpleDateFormat("HH:mm", Locale.ENGLISH).format(datetime)
         }
-        return magnitudeColorResourceId
+
+        private fun getMagnitudeColor(magnitude: Double): Int {
+            val magnitudeColorResourceId: Int
+            val magnitudeFloor = Math.floor(magnitude).toInt()
+            magnitudeColorResourceId = when (magnitudeFloor) {
+                0, 1 -> R.color.magnitude1
+                2 -> R.color.magnitude2
+                3 -> R.color.magnitude3
+                4 -> R.color.magnitude4
+                5 -> R.color.magnitude5
+                6 -> R.color.magnitude6
+                7 -> R.color.magnitude7
+                8 -> R.color.magnitude8
+                9 -> R.color.magnitude9
+                else -> R.color.magnitude10plus
+            }
+            return magnitudeColorResourceId
+        }
     }
 }
